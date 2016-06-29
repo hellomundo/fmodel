b11302597d7cbf1be775e8a9b3c88f2f2ffdaa84
hellomundo <ed.ortega@gmail.com>
2016-06-28 18:55:37 -0700

    added susy lib

--- views.py
+++ views.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello Mundo!"
+    title = "Happy FinApp"
+    return render_template('index.html', title = title)
